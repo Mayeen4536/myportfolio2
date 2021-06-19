87ade194ad001a73061c7fa73c82adc3d45b122e
Mayeen4536 <63101853+Mayeen4536@users.noreply.github.com>
2021-06-20 02:27:48 +0600

    added few animations with gsap

--- script.js
+++ script.js
@@ -6,5 +6,10 @@ links.forEach((item)=>{
         let el = document.getElementById(item.getAttribute('data-link'));
         console.log(el);
         el.scrollIntoView({behavior:"smooth", block:"start"});
+        gsap.from(el,{opacity: -1, duration: 2, x:-100})
     })
+})
+
+links.forEach((item)=>{
+    gsap.from()
 })
